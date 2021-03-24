0bf330bdd640263943e94f8b76873e1eab2ae1fc
alexhein189 <zcabhei@ucl.ac.uk>
2021-03-24 20:38:51 +0000

    app.py commented

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import json
 
+#Imported spiders which were created for each article
 from projectdev.projectdev.spiders.edition import Edition
 from projectdev.projectdev.spiders.escape import Escape
 from projectdev.projectdev.spiders.cntravelerkudos import Cntravelerkudos
@@ -16,8 +17,10 @@ from multiprocessing import Process
 path = os.path.dirname(__file__)
 app = Flask(__name__)
 
+#list that contains the name of the json files
 file_list = ["/edition.json","/escape.json","/cntravelerkudos.json","/cntraveler.json","/highlightszimmermann.json","/monocle.json"]
 
+#methods that start each CrawlerProcess for each spider for every article
 def start_edition_process():
     process = CrawlerProcess()
     process.crawl(Edition)
@@ -49,16 +52,20 @@ def start_monocle_process():
     process.start()
 
 
-
+#route to the main page of the web application
 @app.route('/analysis')
 def hello_world():
     if __name__=="__main__":
         
+        #list of processes instantiated from each article using the above methods
         process_list_1 = []
+        
+        #Process library is used to make sure that the methods for 
+        #creating the CrawlerProcess for each spider runs asynchronously 
             
         p1=Process(target=start_edition_process)
         process_list_1.append(p1)
-        # p1.start() #starts the process which means script is running by itself 
+        #starts the process which means script is running by itself 
         #and in the back the start_edidition_process() function running in parallel
 
         p2=Process(target=start_escape_process)
@@ -77,15 +84,20 @@ def hello_world():
         p6=Process(target=start_monocle_process)
         process_list_1.append(p6)
         
+        #loop iteratres through the list and starts each process asynchronously
         for p in process_list_1:
             p.start()
-            
+        
+        #join() makes sure that the code executes the component that loads the contents onto data only when all the
+        #crawler processes are done    
         for p in process_list_1:
             p.join()
         
 
     data_list=[]
     
+    #iteratres through the file_list that contains the JSON file names in order to load the data and append it to
+    #the data list
     for file in file_list:
         with open(path+file) as items_file:
             data=json.load(items_file)
